efe0edb5fc563a2bf0c708720bcbf06c20326a9e
Nghia Tran <vinhnghiatran@gmail.com>
2024-02-02 15:14:08 +0700

    Init project

--- WebsocketWithProxyman/ViewController.swift
+++ WebsocketWithProxyman/ViewController.swift
@@ -7,13 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, URLSessionWebSocketDelegate {
+
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var sendMessageBtn: UIButton!
+    @IBOutlet weak var statusLbl: UILabel!
+
+    private var count = 0
+    private var isConnected = false {
+        didSet {
+            startBtn.isEnabled = !isConnected
+            sendMessageBtn.isEnabled = isConnected
+            statusLbl.text = "Status: \(isConnected ? "Connected ✅" : "Not Connect")"
+        }
+    }
+
+    private lazy var session: URLSession = {
+        return URLSession(configuration: .default, delegate: self, delegateQueue: .main)
+    }()
+    private lazy var webSocketTask: URLSessionWebSocketTask = {
+        return session.webSocketTask(with: URL(string: "wss://ws.postman-echo.com/raw")!)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.isConnected = false
     }
 
+    @IBAction func startConnectionBtnOnTap(_ sender: Any) {
+        webSocketTask.resume()
+        listen()
+    }
 
+    @IBAction func sendWSBtnOnTap(_ sender: Any) {
+        send(text: "Hello World from Proxyman: Count = \(count)")
+
+        count += 1
+    }
+
+    func listen()  {
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    print("Received: \(text)")
+                case .data(let data):
+                    print("Received: Raw Data: \(data.count)")
+                @unknown default:
+                    fatalError()
+                }
+
+                self.listen()
+            }
+        }
+    }
+
+    func send(text: String) {
+        webSocketTask.send(URLSessionWebSocketTask.Message.string(text)) { error in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol _protocol: String?) {
+        print("didOpenWithProtocol = \(_protocol)")
+        self.isConnected = true
+    }
+
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        print("Closed: \(closeCode)")
+        self.isConnected = false
+    }
 }
 
